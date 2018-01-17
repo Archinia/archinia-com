18914f0f35ca762909fe43a499ce7492414124ef
Melanie E Magdalena <info@melaniemagdalena.com>
2018-01-16 23:57:16 -0700

    move script

--- assets/js/main.js
+++ assets/js/main.js
@@ -323,7 +323,19 @@ var settings = {
 							$image.hide();
 
 					});
-
+			// Kudos
+			$('.kudos').slick({
+		    dots: true,
+		    infinite: true,
+		    autoplay: true,
+		    autoplaySpeed: 6000,
+		    speed: 800,
+		    slidesToShow: 1,
+		    adaptiveHeight: true
+		  });
+		  $( document ).ready(function() {
+		    $('.no-fouc').removeClass('no-fouc');
+		  });
 	});
 
 })(jQuery);
