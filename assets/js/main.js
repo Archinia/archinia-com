ab635a9a8c9266f580c747768439b19c5b55d838
Melanie E Magdalena <info@melaniemagdalena.com>
2018-01-17 19:30:58 -0700

    remove cdn

--- assets/js/main.js
+++ assets/js/main.js
@@ -317,16 +317,6 @@ var settings = {
 							$image.hide();
 
 					});
-				// Kudos
-					$('#kudos')({
-				    dots: true,
-				    infinite: true,
-				    autoplay: true,
-				    autoplaySpeed: 6000,
-				    speed: 800,
-				    slidesToShow: 1,
-				    adaptiveHeight: true
-				  });
 	});
 
 })(jQuery);
