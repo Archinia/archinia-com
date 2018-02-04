c66c7246b0bb4909091d7ce708479c57b774716e
Melanie E Magdalena <info@melaniemagdalena.com>
2018-02-03 19:50:40 -0700

    gulp it
    gulp task for minify css + rename to *.min.css

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+// include plugins
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+gulp.task('default', function () {
+    gulp.src('assets/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('assets/css'));
+});
