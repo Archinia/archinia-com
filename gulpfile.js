5e5a4a54b7d49457efdc36d454af29fb27bb3834
Melanie E Magdalena <info@melaniemagdalena.com>
2018-02-11 20:05:56 -0700

    edits: text + pics

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 
 gulp.task('default', function () {
-    gulp.src('assets/**/*.css')
+    gulp.src('assets/css/main.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('assets/css'));
