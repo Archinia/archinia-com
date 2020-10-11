030db29148f31f930d7c092cd73617a4f0f5c270
Melanie E Magdalena <hello@melaniemagdalena.com>
2020-10-10 23:55:49 -0500

    11tyify

---
+++ .eleventy.js
@@ -0,0 +1,144 @@
+const fs = require('fs');
+
+// 11ty Files
+const filters = require('./site/_11ty/filters.js');
+const shortcodes = require('./site/_11ty/shortcodes.js');
+const collections = require('./site/_11ty/collections.js');
+const transforms = require('./site/_11ty/transforms.js');
+
+// Markdown Plugins
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const markdownItCollapsible = require('markdown-it-collapsible');
+const markdownItFootnote = require('markdown-it-footnote');
+const markdownItImageSize = require('markdown-it-imsize');
+const markdownItToc = require('markdown-it-toc-done-right');
+const markdownItVideo = require('markdown-it-video');
+
+// 11ty Plugins
+const pluginReadingTime = require('eleventy-plugin-reading-time');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+
+module.exports = function (eleventyConfig) {
+
+  /**
+   * Add Collections
+   * @link https://www.11ty.dev/docs/collections/
+   */
+  Object.keys(collections).forEach(collectionName => {
+    eleventyConfig.addCollection(collectionName, collections[collectionName])
+  });
+
+  /**
+   * Add Filters
+   * @link https://www.11ty.io/docs/filters/
+   */
+  Object.keys(filters).forEach(filterName => {
+    eleventyConfig.addFilter(filterName, filters[filterName])
+  });
+
+  /**
+   * Add Shortcodes
+   * @link https://www.11ty.io/docs/shortcodes/
+   */
+  Object.keys(shortcodes).forEach(shortcodeName => {
+    eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName])
+  });
+
+  /**
+   * Add Transforms
+   * @link https://www.11ty.io/docs/config/#transforms
+   */
+  Object.keys(transforms).forEach(transformName => {
+    eleventyConfig.addTransform(transformName, transforms[transformName])
+  });
+
+  /**
+   * Add plugins
+   * @link https://www.11ty.dev/docs/plugins/
+   */
+  eleventyConfig.addPlugin(pluginReadingTime);
+  eleventyConfig.addPlugin(pluginRss);
+
+  /**
+   * Set custom markdown library instance
+   * @link https://www.11ty.dev/docs/languages/liquid/#optional-set-your-own-library-instance
+   */
+  eleventyConfig.setLibrary('md',
+      markdownIt({
+        html: true,
+        breaks: true,
+        typographer: true
+      }).use(markdownItAnchor, {
+        permalink: true,
+        permalinkSymbol: '#',
+        permalinkClass: 'bookmark',
+        permalinkBefore: true,
+        level: 2
+      }).use(markdownItCollapsible
+       ).use(markdownItImageSize, {
+        autofill: true
+      }).use(markdownItToc, {
+        containerClass: 'md-toc',
+        containerId: 'md-toc',
+        level: 2
+      }).use(markdownItVideo, {
+        youtube: { width: 640, height: 390 },
+        vimeo: { width: 500, height: 281 },
+        vine: { width: 600, height: 600, embed: 'simple' },
+        prezi: { width: 550, height: 400 }
+      }).use(markdownItFootnote)
+  );
+
+  /**
+   * Passthrough file copy
+   * @link https://www.11ty.io/docs/copy/
+   */
+  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('images');
+  eleventyConfig.addPassthroughCopy('files');
+
+  /**
+   * Opts in to a full deep merge when combining the Data Cascade.
+   * @link https://www.11ty.dev/docs/data-deep-merge/#data-deep-merge
+   */
+  eleventyConfig.setDataDeepMerge(true);
+
+  /**
+   * Override BrowserSync Server options
+   * @link https://www.11ty.dev/docs/config/#override-browsersync-server-options
+   */
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('dist/404/index.html');
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+    ui: false,
+    ghostMode: false
+  });
+
+  /**
+   * Configuration
+   * @link https://www.11ty.dev/docs/config/#configuration-options
+   */
+  return {
+    dir: {
+      input: 'site',
+      data: '_data',
+      includes: '_includes',
+      layouts: '_layouts',
+      output: 'dist' // publish directory
+    },
+    passthroughFileCopy: true,
+    templateFormats : ['njk', 'md', 'html', '11ty.js'],
+    htmlTemplateEngine : 'njk',
+    markdownTemplateEngine : 'njk',
+  };
+
+};
---
+++ site/pages/disclaimer.md
@@ -0,0 +1,19 @@
+---
+title: Disclaimer of Endorsement
+slug: disclaimer
+featured_image:
+layout: text-only.njk
+---
+The documents posted on this Website contain external links to information created and maintained by other organizations. These links are provided for the user’s convenience. Rachel Preston Prinz, Archinia, and Architecture for Everybody do not control or guarantee the accuracy, relevance, timeliness, or completeness of this outside information. The inclusion of links is not intended to reflect their importance, nor is it intended as an endorsement by Rachel Preston Prinz, Archinia, and Architecture for Everybody of any views expressed or products or services offered by the organizations sponsoring the links. Rachel Preston Prinz, Archinia, and Architecture for Everybody does not endorse individual vendors, products, or services. Therefore, any reference herein to any vendor, product, or services by trade name, trademark, manufacturer, or otherwise does not constitute or imply the endorsement, recommendation, or approval by Rachel Preston Prinz, Archinia, and/or Architecture for Everybody.
+
+<hr class="major" />
+
+## Copyright
+
+All Website materials, including, without limitation, the logos, design, text, graphics, other files and the selection and arrangement are Copyright &copy; Rachel Preston Prinz, Archinia, and Architecture for Everybody or copyrighted as otherwise noted on the page.  The text and graphic content of the Rachel Preston Prinz, Archinia, and Architecture for Everybody websites may be electronically copied and printed as hard copy only under the following conditions:</p>
+
+- The content is used for informational and non-commercial use only;
+- Copyright information is displayed (e.g. &copy; Copyright 2017 Architecture for EveryBody. All Rights Reserved); and/or
+- The content is not modified.
+
+Any other use of the materials on this Website, including any commercial use, reproduction for purposes other than as described above, distribution, republication, or display performed without prior written permission of Rachel Preston Prinz, Archinia, and/or Architecture for Everybody is strictly prohibited.
